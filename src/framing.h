3c542211eb7182f9f1090568bf65e290d560be8b
LorenzoTrick <57558456+LorenzoTrick@users.noreply.github.com>
2019-11-27 19:03:12 +0100

    remodeled byte_stuffing, added remove_byte_stuffing, fixed bugs

--- src/framing.h
+++ src/framing.h
@@ -20,24 +20,24 @@
  * @param f Oggetto @c frame in cui aggiungere bit_stuffing.
  * @return true Se l'operazione è andata a buon termine.
  * @return false Se la grandezza supera quella del MAXEL.
- *               In pratica se f.size è pari o superiore a 255.
+ *               In pratica se f.size è pari o superiore a 256 - i flag = 254.
  */
 bool bit_stuffing(frame &f)
 {
     bool ans = true;
 
-    if (f.size < FRAME_MAXEL - 1)
+    if (f.size < FRAME_MAXEL - 2)
     {
         // Inserisce flag all'inizio
         insert_at(f.message, f.size, 0, (char)FLAG);
 
         //Cambia bit se vede un flag da mandare come messaggio
-        for (int i = 1; i <= f.size; i++)
+        for (int i = 1; i < f.size; i++)
             if (f.message[i] == (char)FLAG)
                 f.message[i] -= 2; // Cambia penultimo bit
 
         // Inserisce flag alla fine
-        f.message[f.size++] = (char)FLAG;
+		insert_at(f.message, f.size, f.size, (char)FLAG);
     }
     else
         ans = false;
@@ -66,30 +66,43 @@ void remove_bit_stuffing(frame &f)
  * 
  * @param f Oggetto @c frame in cui aggiungere byte stuffing.
  * @return true Se l'operazione è andata a buon termine.
- * @return false Se il primo o l'ultimo elemento sono già pieni.
- * 
- * @todo Usare i metodi delete_at e insert_at in modo da evitare di dover
- *       lasciare uno spazio vuoto nel vettore per far posto al byte stuffing.
- *       Guardare la funzione bit stuffing per essere sicuri.
+ * @return false Se la grandezza supera quella del MAXEL.
+
+ * @todo Fare in modo che la duplicazione dei caratteri restituisca ans = false se si é andato oltre il MAXEL.
  */
 bool byte_stuffing(frame &f)
 {
     bool ans = true;
 
-    if (f.message[0] != '\0' || f.message[1] != '\0' || f.message[f.size + 1] != '\0' || f.message[f.size + 2] != '\0')
-        ans = false;
-    else if (f.size <= FRAME_MAXEL - 2)
-    {
-        f.message[0] = ESC;
-        f.message[1] = STX;
-        f.message[f.size + 1] = ETX;
-        f.message[f.size + 2] = ESC;
-        f.size += 2;
-    }
-    else //come bit_stuffing
-        ans = false;
+	if (f.size < FRAME_MAXEL - 4)
+	{
+		//Inserisce caratteri di controllo all'inizio
+		insert_at(f.message, f.size, 0, ESC);
+		insert_at(f.message, f.size, 1, STX);
 
-    //todo: duplica caratteri in f.message
+		//Duplica se vede un carattere di controllo come messaggio
+		for (int i = 2; i < f.size; i++)
+			if (is_special_char(f.message[i]));
+			{
+				insert_at(f.message, f.size, i, f.message[i]);
+				i++; //va oltre il carattere appena duplicato
+			}
+
+		//Inserisce caratteri di controllo alla fine
+		insert_at(f.message, f.size, f.size, ESC);
+		insert_at(f.message, f.size, f.size, ETX);
+	}
+	else
+		ans = false;
 
     return ans;
+}
+
+void remove_bit_stuffing(frame &f)
+{
+	for (int i = 0; i < f.size; i++)
+	{
+		if (is_special_char(f.message[i]) && f.message[i + 1] == f.message[i])
+			delete_at(f.message, f.size, i);
+	}
 }
